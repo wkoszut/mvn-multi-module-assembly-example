086a50a5f7f5365db8ca0afcd04fb1e931586474
wkoszut_camo_code <wkoszut@gmail.com>
2020-01-22 09:14:53 +0100

    Remove log4j

--- standalone/src/main/java/com/synapsis/mvn/App.java
+++ standalone/src/main/java/com/synapsis/mvn/App.java
@@ -1,20 +1,15 @@
 package com.synapsis.mvn;
 
-import org.apache.log4j.Logger;
-
-/** Hello world! */
+/**
+ * Hello world!
+ */
 public class App {
-
-    private final Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         new App().logSomething();
     }
 
     public boolean logSomething() {
-        logger.info("Start....");
-
-        logger.info("End....");
         return true;
     }
 }
